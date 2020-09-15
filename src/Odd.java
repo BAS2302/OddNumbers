4e8010c5d96871b397294dc4cf8349365bc3c9a3
BelAnton <belanton@mail.ru>
2020-09-15 21:56:22 +0500

    methods vain and printOdd

---
+++ src/Odd.java
@@ -0,0 +1,11 @@
+public class Odd {
+    public static void main(String[] args) {
+        int[] array = {15, 34, 5, 3, -11, 567, 2, 547, 2, 0, -12, 35};
+        printOdd(array);
+    }
+
+    public static String printOdd(int[] array) {
+        int[] arrayResult = new int[array.length];
+
+    }
+}
